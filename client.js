ac37179d9a0aabae0cdd8b4b87d0440ebd4dba62
Adnan Mohamud <adnanmohamud6@gmail.com>
2023-10-05 14:13:47 -0700

    added in initial connection to server

---
+++ client.js
@@ -0,0 +1,36 @@
+const io = require('socket.io-client');
+
+const serverUrl = 'http://localhost:3002/tasks'; 
+
+const socket = io(serverUrl);
+
+// Replace 'ClientName' with the actual client name
+const clientName = 'ClientName';
+
+socket.on('connect', () => {
+  console.log(`Connected to the hub as ${socket.id}`);
+
+  // Join the client to their own room
+  socket.emit('join', { assignee: clientName });
+
+  // Subscribe to the 'getTasks' event to receive the list of tasks
+  socket.on('getTasks', (tasks) => {
+    console.log(`Received tasks for ${clientName}:`, tasks);
+  });
+
+  // Example: Create a new task
+  const newTask = { title: 'New Task' };
+  socket.emit('create', { assignee: clientName, task: newTask });
+
+  // Example: Update existing tasks
+  const updatedTask = { title: 'Task 1', completed: true };
+  socket.emit('update', { assignee: clientName, task: updatedTask });
+
+  // Example: Delete tasks
+  const taskToDelete = { title: 'Task 3' };
+  socket.emit('delete', { assignee: clientName, task: taskToDelete });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from the hub');
+});
